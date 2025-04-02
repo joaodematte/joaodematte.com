509bd198e6a6017ea86e09e9720ac59328439c50
João Dematte <jgdematte@gmail.com>
2025-04-01 21:59:38 -0300

    og image from post

---
+++ src/app/writing/[slug]/opengraph-image.tsx
@@ -0,0 +1,92 @@
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { ImageResponse } from 'next/og';
+
+import { getPost } from '@/lib/posts';
+
+interface Props {
+  params: { slug: string };
+}
+
+export const alt = 'joão dematte';
+export const size = {
+  width: 1200,
+  height: 630
+};
+
+export const contentType = 'image/png';
+
+export default async function Image({ params }: Props) {
+  const lora = await readFile(join(process.cwd(), 'public/Lora-Regular.ttf'));
+  const loraBold = await readFile(join(process.cwd(), 'public/Lora-Bold.ttf'));
+
+  const post = await getPost(params.slug);
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          position: 'relative',
+          background: '#fafafa',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+          justifyContent: 'flex-end'
+        }}
+      >
+        <div
+          style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            opacity: 0.05,
+            backgroundImage: "url('https://joaodematte.com/noise.png')"
+          }}
+        />
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            padding: 64
+          }}
+        >
+          <h1
+            style={{
+              fontSize: 64,
+              fontWeight: 600,
+              marginBottom: -12
+            }}
+          >
+            {post?.title}
+          </h1>
+          <p
+            style={{
+              fontSize: 24
+            }}
+          >
+            {post?.description}
+          </p>
+        </div>
+      </div>
+    ),
+    {
+      ...size,
+      fonts: [
+        {
+          name: 'Lora',
+          data: lora,
+          style: 'normal',
+          weight: 400
+        },
+        {
+          name: 'Lora',
+          data: loraBold,
+          style: 'normal',
+          weight: 600
+        }
+      ]
+    }
+  );
+}
